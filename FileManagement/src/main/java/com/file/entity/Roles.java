3552eedc742a8bd6f3bcf28eee74fc20f12dde3b
Rahul <sosarahul0000@gmail.com>
2025-03-07 15:32:04 +0530

    add roles entity

---
+++ FileManagement/src/main/java/com/file/entity/Roles.java
@@ -0,0 +1,50 @@
+package com.file.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Roles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<Users> users = new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public Set<Users> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<Users> users) {
+		this.users = users;
+	}
+    
+    
+
+}
