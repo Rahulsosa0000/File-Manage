4713ee041f9df9cdebcf8f04684a59bae56515a7
Shreya <shreya.ravat.ait@gmail.com>
2025-03-07 14:51:23 +0530

    added message

--- FileManagement/src/main/java/com/file/FileManagementApplication.java
+++ FileManagement/src/main/java/com/file/FileManagementApplication.java
@@ -8,6 +8,8 @@ public class FileManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileManagementApplication.class, args);
+		
+		System.out.println("howr u");
 	}
 
 }
