a5bc845b31c03d152138fef204f00b6c29eec0d3
Rahul <sosarahul0000@gmail.com>
2025-03-07 15:58:43 +0530

    Entity add

--- FileManagement/src/main/java/com/file/entity/Roles.java
+++ FileManagement/src/main/java/com/file/entity/Roles.java
@@ -1,13 +1,9 @@
 package com.file.entity;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Roles {
@@ -16,35 +12,22 @@ public class Roles {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String roleName;
+    private String roleName;  // Example: Admin, Manager, Employee
 
-    @ManyToMany(mappedBy = "roles")
-    private Set<Users> users = new HashSet<>();
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public String getRoleName() {
+        return roleName;
+    }
 
-	public String getRoleName() {
-		return roleName;
-	}
-
-	public void setRoleName(String roleName) {
-		this.roleName = roleName;
-	}
-
-	public Set<Users> getUsers() {
-		return users;
-	}
-
-	public void setUsers(Set<Users> users) {
-		this.users = users;
-	}
-    
-    
-
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 }
--- FileManagement/src/main/java/com/file/entity/Users.java
+++ FileManagement/src/main/java/com/file/entity/Users.java
@@ -1,77 +1,57 @@
 package com.file.entity;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import jakarta.persistence.FetchType;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Users {
-	
-	@Id
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-	
-	private String username;
-	
-	private String password;
-	
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-      name = "user_roles", 
-      joinColumns = @JoinColumn(name = "user_id"), 
-      inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Roles> roles = new HashSet<>();
+    private String username;
+    private String password;
 
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Roles role;
 
-	public Long getId() {
-		return id;
-	}
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public Roles getRole() {
+        return role;
+    }
 
-
-	public String getPassword() {
-		return password;
-	}
-
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-
-	public Set<Roles> getRoles() {
-		return roles;
-	}
-
-
-	public void setRoles(Set<Roles> roles) {
-		this.roles = roles;
-	}
-    
-    
-	
-	
-	
-
+    public void setRole(Roles role) {
+        this.role = role;
+    }
 }
+
